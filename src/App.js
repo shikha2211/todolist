92bdbc94c0bc0010788decf30764bf1e6a053f6a
shikha2211 <sk.shikha@gmail.com>
2024-01-03 19:38:00 +0000

    Checking in code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let id = 0;
+
+  const oldTask = [
+    {id: id++ , name:'Wash clothes'},
+    {id: id++, name:'Withdraw Money'},
+    {id: id++ , name:'Buy Groceries'}
+  ];
+  const [task,setTask] = useState(oldTask);
+  const [newtask, setnewTask] = useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <ul>
+          {
+            task.map(({id, name}) => 
+              <li key={id}>{name}
+          <button 
+              onClick = {() => 
+              setTask(
+                task.filter((task)=> task.id !== id))
+          }
+          >Delete
+          </button>
+              </li>
+            )
+          }
+        </ul>
+      </p>
+      <p>
+      <input type="text" placeholder='Enter a task' 
+      onChange=
+      {(e) =>
+        setnewTask(e.target.value)
+      } />
+      <input type="button" value="Submit" onClick = {(e) =>
+        setTask(
+          task.concat({
+            id: id++,
+            name: newtask.trim()
+          })
+        )
+      } />
+      </p>
     </div>
   );
 }
